05aa3aa84037ba4615524a050b14f62b7cb6a5fc
SWETA <pradhansweta582@gmail.com>
2021-06-08 12:43:27 +0530

    new

---
+++ app/src/main/java/com/mynewsapp/mynews/ui/cnn/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.mynewsapp.mynews.ui.cnn;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
